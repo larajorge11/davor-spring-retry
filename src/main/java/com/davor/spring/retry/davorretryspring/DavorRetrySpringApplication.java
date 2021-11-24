92ed27392b6804e01cdfa3a2570f3788499d4752
Jorge Lara <larajorge11@gmail.com>
2021-11-23 20:25:21 -0500

    Initial commit

---
+++ src/main/java/com/davor/spring/retry/davorretryspring/DavorRetrySpringApplication.java
@@ -0,0 +1,13 @@
+package com.davor.spring.retry.davorretryspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavorRetrySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavorRetrySpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davor/spring/retry/davorretryspring/DavorRetrySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davor.spring.retry.davorretryspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavorRetrySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
