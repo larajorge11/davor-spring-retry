004bdc3b78a9759a50fa01630bb0b68a5eda14e5
Jorge Lara <larajorge11@gmail.com>
2021-11-23 20:39:29 -0500

    chore(Adding maven dependencies and annotation)

--- src/main/java/com/davor/spring/retry/davorretryspring/DavorRetrySpringApplication.java
+++ src/main/java/com/davor/spring/retry/davorretryspring/DavorRetrySpringApplication.java
@@ -2,7 +2,9 @@ package com.davor.spring.retry.davorretryspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
+@EnableRetry
 @SpringBootApplication
 public class DavorRetrySpringApplication {
 
